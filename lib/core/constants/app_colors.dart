99e5dacb5224a53c619440d13c750c7e89ac5b4a
Rafsan <rapsanza@gmail.com>
2025-05-17 13:48:18 +0700

    feat: add color and text style constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Primary Colors
+  static const Color primary = Color(0xFF185DCF);
+  static const Color yellow = Color(0xFFFDBD31);
+
+  // Neutral Colors
+  static const Color black = Color(0xFF2E2E2E);
+  static const Color white = Color(0xFFFFFFFF);
+  
+  // Grey Colors
+  static const Color greyPrimary = Color(0xFF969696);
+  static const Color greySecondary = Color(0xFFACACAC);
+  
+  // Background
+  static const Color background = Color(0xFFF0F4FF);
+}
---
+++ lib/core/constants/app_text_styles.dart
@@ -0,0 +1,191 @@
+
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  // Caption2
+  static const TextStyle caption2Regular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 11,
+    height: 13 / 11,
+    letterSpacing: 0.07,
+  );
+  
+  static const TextStyle caption2Bold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w600,
+    fontSize: 11,
+    height: 13 / 11,
+    letterSpacing: 0.06,
+  );
+
+  // Caption1
+  static const TextStyle caption1Regular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 12,
+    height: 16 / 12,
+  );
+  
+  static const TextStyle caption1Bold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    height: 16 / 12,
+  );
+
+  // Footnote
+  static const TextStyle footnoteRegular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 13,
+    height: 18 / 13,
+    letterSpacing: -0.08,
+  );
+  
+  static const TextStyle footnoteBold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+    height: 18 / 13,
+    letterSpacing: -0.08,
+  );
+
+  // Subheadline
+  static const TextStyle subheadlineRegular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 15,
+    height: 20 / 15,
+    letterSpacing: -0.24,
+  );
+  
+  static const TextStyle subheadlineBold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w600,
+    fontSize: 15,
+    height: 20 / 15,
+    letterSpacing: -0.5,
+  );
+
+  // Callout
+  static const TextStyle calloutRegular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 16,
+    height: 21 / 16,
+    letterSpacing: -0.32,
+  );
+  
+  static const TextStyle calloutBold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 21 / 16,
+    letterSpacing: -0.32,
+  );
+
+  // Body
+  static const TextStyle bodyRegular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 17,
+    height: 22 / 17,
+    letterSpacing: -0.41,
+  );
+  
+  static const TextStyle bodyBold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w600,
+    fontSize: 17,
+    height: 22 / 17,
+    letterSpacing: -0.41,
+  );
+
+  // Headline
+  static const TextStyle headlineRegular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w600,
+    fontSize: 17,
+    height: 22 / 17,
+    letterSpacing: -0.41,
+  );
+  
+  static const TextStyle headlineBold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w600,
+    fontSize: 17,
+    height: 22 / 17,
+    letterSpacing: -0.41,
+  );
+
+  // Title3
+  static const TextStyle title3Regular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 20,
+    height: 25 / 20,
+    letterSpacing: 0.38,
+  );
+  
+  static const TextStyle title3Bold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    height: 25 / 20,
+    letterSpacing: 0.38,
+  );
+
+  // Title2
+  static const TextStyle title2Regular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 22,
+    height: 28 / 22,
+    letterSpacing: 0.35,
+  );
+  
+  static const TextStyle title2Bold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.bold,
+    fontSize: 22,
+    height: 28 / 22,
+    letterSpacing: 0.35,
+  );
+
+  // Title1
+  static const TextStyle title1Regular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 28,
+    height: 34 / 28,
+    letterSpacing: 0.36,
+  );
+  
+  static const TextStyle title1Bold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.bold,
+    fontSize: 28,
+    height: 34 / 28,
+    letterSpacing: 0.36,
+  );
+
+  // LargeTitle
+  static const TextStyle largeTitleRegular = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 34,
+    height: 41 / 34,
+    letterSpacing: 0.37,
+  );
+  
+  static const TextStyle largeTitleBold = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.bold,
+    fontSize: 34,
+    height: 41 / 34,
+    letterSpacing: 0.37,
+  );
+}
