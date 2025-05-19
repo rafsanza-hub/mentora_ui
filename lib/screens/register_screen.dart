a2994568f6632614fff150d9e6894e9970bb6086
Rafsan <rapsanza@gmail.com>
2025-05-19 20:05:35 +0700

    feat: add RegisterScreen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -3,10 +3,9 @@ import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:get/get.dart';
 import 'package:mentora/core/constants/app_colors.dart';
 import 'package:mentora/core/constants/app_text_styles.dart';
-import 'package:mentora/screens/register_screen.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -19,70 +18,90 @@ class LoginScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 const SizedBox(height: 90),
-                Text('Login', style: AppTextStyles.title2Bold),
+                Text('Create Account', style: AppTextStyles.title2Bold),
                 SizedBox(height: 20),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25),
                   child: Text(
-                    'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over',
+                    'You must first create an account to be able to access the LearnConnect platform and be able to enjoy the various available learning features.',
                     style: AppTextStyles.caption1Regular,
                     textAlign: TextAlign.center,
                   ),
                 ),
                 SizedBox(height: 40),
+
+                // Form
                 Form(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
+                      // Full Name
                       TextFormField(
                         decoration: InputDecoration(
                           prefixIcon: Icon(IconlyLight.profile),
+                          labelText: 'Full Name',
+                        ),
+                      ),
+                      const SizedBox(height: 14),
+
+                      // Email
+                      TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(IconlyLight.message),
                           labelText: 'Your Email',
                         ),
                       ),
                       const SizedBox(height: 14),
+
+                      // Password
                       TextFormField(
                         decoration: InputDecoration(
                           prefixIcon: Icon(IconlyLight.password),
                           labelText: 'Password',
                         ),
                       ),
-                      const SizedBox(height: 12),
-                      Text(
-                        'Forgot Password?',
-                        style: AppTextStyles.footnoteRegular.copyWith(
-                          color: AppColors.primary,
+                      const SizedBox(height: 14),
+
+                      // Confirm Password
+                      TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(IconlyLight.password),
+                          labelText: 'Confirm Password',
                         ),
-                        textAlign: TextAlign.right,
                       ),
-                      const SizedBox(height: 20),
+                      const SizedBox(height: 24),
+
+                      // Register Button
                       ElevatedButton(
                         child: const Text(
-                          'Login',
+                          'Create Account',
                           style: TextStyle(color: AppColors.white),
                         ),
                         onPressed: () {},
                       ),
                       const SizedBox(height: 12),
-                      OutlinedButton(
-                        onPressed: () {
-                          Get.to(() => const RegisterScreen());
-                        },
-                        child: const Text('Create Account'),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 22),
+                        child: Text(
+                          'By registering, it means that you agree to the Terms and Conditions that apply.',
+                          style: AppTextStyles.caption1Regular,
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                      SizedBox(height: 30),
+                      SizedBox(height: 24),
                       Divider(),
                       SizedBox(height: 24),
                       ElevatedButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Login with Google',
-                            style: TextStyle(color: AppColors.black),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColors.white,
-                            shadowColor: Colors.black.withOpacity(0.3),
-                          ))
+                        onPressed: () {},
+                        child: const Text(
+                          'Google',
+                          style: TextStyle(color: AppColors.black),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.white,
+                          shadowColor: Colors.black.withOpacity(0.3),
+                        ),
+                      )
                     ],
                   ),
                 ),
